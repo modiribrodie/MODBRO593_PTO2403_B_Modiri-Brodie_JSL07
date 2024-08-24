fe5014d78cd6269fc7abedaf325c85decbd68538
modiribrodie <163874746+modiribrodie@users.noreply.github.com>
2024-08-24 23:10:28 +0200

    Dynamic content & Modal
    Content generated
    
    This will close the modal when the button is clicked.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
+      const personalMessageInput =personal ;
       const courseNameInput = ; 
   
       const studentName = studentNameInput.value;
@@ -27,6 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
       // 🚨 Generate certificate content dynamically
       certificateContent. = `
       <h3>${studentName}</h3>
+      <p>${personalMessage}</p>
+      <p>Course: ${courseName}</p>
     `;
     
       //  Display the modal
@@ -39,8 +41,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function () {
+    modal.style.display = 'none';  
     });
   });
   
